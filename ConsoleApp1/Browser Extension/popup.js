2d390a5f1a1d1e67fe5c42dcc9eda8ab5fd7d82d
Igor Markiv <im@s4h.pl>
2023-12-28 11:36:22 +0100

    added download Html by click

--- ConsoleApp1/Browser Extension/popup.js
+++ ConsoleApp1/Browser Extension/popup.js
@@ -1,15 +1,15 @@
-﻿//// popup.js
-//document.getElementById('downloadBtn').addEventListener('click', () => {
-//    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-//        var activeTab = tabs[0];
-//        chrome.scripting.executeScript({
-//            target: { tabId: activeTab.id },
-//            function: tabScript
-//        });
-//    });
-//});
+﻿// popup.js
+document.getElementById('downloadBtn').addEventListener('click', () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        var activeTab = tabs[0];
+        chrome.scripting.executeScript({
+            target: { tabId: activeTab.id },
+            function: tabScript
+        });
+    });
+});
 
-//// This function gets injected into the tab and sends back the HTML
-//function tabScript() {
-//    chrome.runtime.sendMessage({ html: document.documentElement.outerHTML });
-//}
+// This function gets injected into the tab and sends back the HTML
+function tabScript() {
+    chrome.runtime.sendMessage({ html: document.documentElement.outerHTML });
+}
